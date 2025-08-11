1c48aebcd24e5872b0c0488fb758cf6506a08cb2
Manu2402 <solanomanuel002@gmail.com>
2025-08-11 15:00:32 +0200

    Moved .git files in order to show just the plugin's folder content

--- LogoVirtualMachine/Source/LogoVirtualMachine/LogoVirtualMachine.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "LogoVirtualMachine.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LogoVirtualMachine, "LogoVirtualMachine" );
